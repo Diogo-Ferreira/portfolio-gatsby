d7b7d09be620a3c6db5a05cb0846f5cde30e0c60
Diogo Ferreira <d.venancio.ferreira@gmail.com>
2019-01-11 17:00:33 +0100

    pwa colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#1C1C1C',
+        theme_color: '#DADDD8',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
